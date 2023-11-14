9d947d1d7bc83ff41f64b071f8d22fd6823cd631
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-14 13:43:27 -0600

    first commit

--- src/ui/MatchTable.jsx
+++ src/ui/MatchTable.jsx
@@ -5,13 +5,13 @@ import MatchRow from "./MatchRow";
 function MatchTable({ setShowModal }) {
   return (
     <div>
-      <div className="flex items-center gap-x-4 rounded-[5px_5px_0px_0px] px-4 py-2">
+      <div className="flex items-center gap-x-4 rounded-[5px_5px_0px_0px]  bg-secondaryColor px-4 py-2">
         <img src={glove} alt="" />
         <h2 className="text-sm font-bold text-white">
           World Boxing Organisation
         </h2>
       </div>
-      <div className="flex flex-col gap-y-[1px] bg-[#c4c4c4]">
+      <div className="flex flex-col gap-y-[1px] ">
         <MatchRow setShowModal={setShowModal} />
         <MatchRow setShowModal={setShowModal} />
         <MatchRow setShowModal={setShowModal} />
