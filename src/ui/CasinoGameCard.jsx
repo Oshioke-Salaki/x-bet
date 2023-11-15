b3ba001a518231d734097586df8346569e660069
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-15 14:56:04 -0600

    update

--- src/ui/CasinoGameCard.jsx
+++ src/ui/CasinoGameCard.jsx
@@ -2,8 +2,8 @@ import bingo from "../assets/bingo.png";
 
 function CasinoGameCard() {
   return (
-    <div className="flex items-center gap-x-5 bg-white p-4 text-black">
-      <img src={bingo} alt="" />
+    <div className="flex items-center gap-x-5 rounded-[10px] bg-white p-4 text-black">
+      <img src={bingo} alt="" className="w-[149px]" />
       <div>
         <h1 className="text-base font-bold">Daily Raffle</h1>
         <p className="text-xs font-bold">Share in the active daily pool</p>
