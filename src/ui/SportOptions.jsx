1f646ecc1d883f705dc8708ff9a72b4719ac99fd
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-15 15:14:52 -0600

    update

--- src/ui/SportOptions.jsx
+++ src/ui/SportOptions.jsx
@@ -1,29 +1,31 @@
 function SportOptions() {
   return (
     <ul className="flex items-center gap-x-[37px] text-sm font-bold text-black">
-      <li className="bg-primaryColor rounded-[20px] px-3 py-[5px]">Boxing</li>
-      <li className="hover:bg-primaryColor hover:text-white cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150">
+      <li className="rounded-[20px] bg-primaryColor px-3 py-[5px] text-white">
+        Boxing
+      </li>
+      <li className="cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150 hover:bg-primaryColor hover:text-white">
         Football
       </li>
-      <li className="hover:bg-primaryColor hover:text-white cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150">
+      <li className="cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150 hover:bg-primaryColor hover:text-white">
         Baseball
       </li>
-      <li className="hover:bg-primaryColor hover:text-white cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150">
+      <li className="cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150 hover:bg-primaryColor hover:text-white">
         Golf
       </li>
-      <li className="hover:bg-primaryColor hover:text-white cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150">
+      <li className="cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150 hover:bg-primaryColor hover:text-white">
         UFC
       </li>
-      <li className="hover:bg-primaryColor hover:text-white cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150">
+      <li className="cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150 hover:bg-primaryColor hover:text-white">
         Basketball
       </li>
-      <li className="hover:bg-primaryColor hover:text-white cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150">
+      <li className="cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150 hover:bg-primaryColor hover:text-white">
         Handball
       </li>
-      <li className="hover:bg-primaryColor hover:text-white cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150">
+      <li className="cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150 hover:bg-primaryColor hover:text-white">
         Hockey
       </li>
-      <li className="hover:bg-primaryColor hover:text-white cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150">
+      <li className="cursor-pointer rounded-[20px] bg-white px-3 py-[5px] transition-all duration-150 hover:bg-primaryColor hover:text-white">
         Handball
       </li>
     </ul>
