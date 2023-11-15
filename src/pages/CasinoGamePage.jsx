00c2eb291e959f2b1c549c1bd91216cbb5c3a15b
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-15 15:43:48 -0600

    update

--- src/pages/CasinoGamePage.jsx
+++ src/pages/CasinoGamePage.jsx
@@ -4,7 +4,7 @@ import TopStakePool from "../ui/TopStakePool";
 function CasinoGamePage() {
   return (
     <div className="grid grid-cols-[990px_auto] gap-x-6">
-      <div className="flex w-full rounded-[5px] bg-white p-6 ">
+      <div className="grid w-full grid-cols-[653px_auto] rounded-[5px] bg-white p-6 ">
         <div className="grid grid-cols-[9] gap-x-2 gap-y-4 bg-ash30 p-6">
           {Array.from({ length: 100 }, (_, index) => index + 1).map(
             (curr, i) => (
