5f1615b137e4232da33a062610969c4d279b3269
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-15 15:40:36 -0600

    update

--- src/ui/CasinoGameCard.jsx
+++ src/ui/CasinoGameCard.jsx
@@ -1,19 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import bingo from "../assets/bingo.png";
 
 function CasinoGameCard() {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center gap-x-5 rounded-[10px] bg-white p-4 text-black">
       <img src={bingo} alt="" className="w-[149px]" />
       <div>
         <h1 className="text-base font-bold">Daily Raffle</h1>
         <p className="text-xs font-bold">Share in the active daily pool</p>
-        <NavLink
-          to="casino/12"
+        <button
           className="mt-[55px] w-full rounded-[5px] bg-primaryColor p-[10px] text-sm font-bold text-white"
+          onClick={() => navigate("casino/12")}
         >
           Play Now
-        </NavLink>
+        </button>
       </div>
     </div>
   );
