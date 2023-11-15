87336ed11cba982a3b2a147ef3c1759936340ed2
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-15 15:11:23 -0600

    update

--- src/pages/CasinoPage.jsx
+++ src/pages/CasinoPage.jsx
@@ -1,15 +1,15 @@
 import CasinoGames from "../ui/CasinoGames";
 import MyBets from "../ui/MyBets";
 import TopStakePool from "../ui/TopStakePool";
+import ballons from "../assets/numberBallons.png";
 
 function CasinoPage() {
   return (
     <div className="grid grid-cols-[990px_auto] gap-x-6">
       {/* {showModal && <StakePoolModal setShowModal={setShowModal} />} */}
       <div className="w-full">
-        {/* <img src={banner} alt="" className="mb-[23.2px] h-[186.67px] w-full" />
-        <StakeTable setShowModal={setShowModal} /> */}
         <CasinoGames />
+        <img src={ballons} alt="" className="mt-6 w-full" />
       </div>
       <div className="flex w-full flex-col gap-y-[22px]">
         <MyBets />
